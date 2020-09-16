bfeba63e7012a66cab12b3af3a06aad8c2901444
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-09-16 11:01:01 -0500

    function complete

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -23,5 +23,9 @@ for(i = 1; i<=10; i++) {
 const num = 3;
 
 function multiplyBy2 (inputNumber) {
-
+ const result = inputNumber*2;
+ return result;
 }
+
+const output = multiplyBy2(num)
+const newOutput = multiplyBy2(10);
